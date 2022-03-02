84643e02ed4957d34bd84c6ec518a265f27b1da4
ShurtEnder <gomezrene1999@gmail.com>
2022-03-01 19:29:18 -0500

    GUI MainMenu/AddCust

--- src/controller/LoginPage.java
+++ src/controller/LoginPage.java
@@ -2,14 +2,13 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.ZoneId;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -24,6 +23,7 @@ public class LoginPage implements Initializable {
     public Label userNameLbl;
     public Label passwordLbl;
     public Button loginBttnTxt;
+    public Label zoneIDLbl;
 
     public String[] stringList(String str){
        String[] splited = str.split("\\s+");
@@ -54,15 +54,24 @@ public class LoginPage implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ResourceBundle rb = getBundle("Lan/Nat", Locale.getDefault());
+        ZoneId zoneID = ZoneId.systemDefault();
+        String strZoneID = zoneID.getId();
+        zoneIDLbl.setText(strZoneID);
+        String loginPage =loginPageLbl.getText();
+        String userName = userNameLbl.getText();
+        String password = passwordLbl.getText();
+        String button = loginBttnTxt.getText();
 
         if(Locale.getDefault().getLanguage().equals("en")) {
             System.out.println(rb.getString("hello") + " " + rb.getString("world"));
-            String loginPage =loginPageLbl.getText();
             String test = "";
             String cap = "";
             cap = loginPage;
             test = loginPage.toLowerCase();
+            /*
             System.out.println(test);
+
+             */
             String[] loginPageList = stringList(test);
             String[] loginPageCapList = stringList(cap);
 
@@ -70,12 +79,18 @@ public class LoginPage implements Initializable {
                 String s = loginPageList[i];
                 String c = loginPageCapList[i];
                 if(isUpperCase(c)){
+                    /*
                     System.out.println("Upper Case!");
+
+                     */
                     String trans = rb.getString(s);
                     char capLet = trans.toUpperCase().charAt(0);
                     char first = trans.charAt(0);
+                    /*
                     System.out.println(capLet);
                     System.out.println(first);
+
+                     */
                     loginPageList[i] = trans.replace(first, capLet);;
                 }
                 else{
@@ -91,7 +106,6 @@ public class LoginPage implements Initializable {
         else if(Locale.getDefault().getLanguage().equals("es")){
             System.out.println(rb.getString("hello") + " " + rb.getString("world"));
             System.out.println(rb.getString("hello") + " " + rb.getString("world"));
-            String loginPage =loginPageLbl.getText();
             String test = "";
             String cap = "";
             cap = loginPage;
@@ -104,12 +118,18 @@ public class LoginPage implements Initializable {
                 String s = loginPageList[i];
                 String c = loginPageCapList[i];
                 if(isUpperCase(c)){
+                    /*
                     System.out.println("Upper Case!");
+
+                     */
                     String trans = rb.getString(s);
                     char capLet = trans.toUpperCase().charAt(0);
                     char first = trans.charAt(0);
+                    /*
                     System.out.println(capLet);
                     System.out.println(first);
+
+                     */
                     loginPageList[i] = trans.replace(first, capLet);;
                 }
                 else{
@@ -127,12 +147,53 @@ public class LoginPage implements Initializable {
     }
 
     public void onActionLoginBttn(ActionEvent actionEvent) throws SQLException {
+        ResourceBundle rb = getBundle("Lan/Nat", Locale.getDefault());
+        String message = "No user or password found!";
+
+        boolean userFound = false;
         ResultSet rs = null;
         Statement stmt = connection.createStatement();
         rs = stmt.executeQuery("SELECT * FROM client_schedule.users");
         while(rs.next()) {
-            String test = rs.getString(2);
-            System.out.println(test);
+            String usernameDB = rs.getString(2);
+            String passwordDB = rs.getString(3);
+            /*
+            System.out.println("DB username: "+ usernameDB);
+            System.out.println("DB password: "+ passwordDB);
+            System.out.println("Text username: "+ userNameTxt.getText());
+            System.out.println("Text password: "+ passwordTxt.getText());
+
+             */
+
+
+            if(usernameDB.equals(userNameTxt.getText()) && passwordDB.equals(passwordTxt.getText())){
+                System.out.println("User found!");
+                userFound = true;
+            }
+
         }
+        if(!userFound){
+            if(Locale.getDefault().getLanguage().equals("en")) {
+                String okStr = rb.getString("OK");
+                ButtonType ok = new ButtonType(okStr, ButtonBar.ButtonData.OK_DONE);
+                Alert alert = new Alert(Alert.AlertType.NONE, message, ok);
+                alert.setTitle("");
+                alert.showAndWait();
+            }
+            else if(Locale.getDefault().getLanguage().equals("es")) {
+                String okStr = rb.getString("OK");
+                ButtonType ok = new ButtonType(okStr, ButtonBar.ButtonData.OK_DONE);
+                Alert alert = new Alert(Alert.AlertType.NONE, message, ok);
+                alert.setTitle("");
+                alert.showAndWait();
+            }
+            else if(Locale.getDefault().getLanguage().equals("fr")) {
+                ButtonType ok = new ButtonType("Ok", ButtonBar.ButtonData.OK_DONE);
+                Alert alert = new Alert(Alert.AlertType.NONE, message, ok);
+                alert.setTitle("");
+                alert.showAndWait();
+            }
+        }
+
     }
 }
